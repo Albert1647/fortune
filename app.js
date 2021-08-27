6b22bfa764d568d6a9fcd6f3de184e6688a2e0ec
Natthadecha Kijsamrej <41380751+Albert1647@users.noreply.github.com>
2021-08-27 16:51:22 +0700

    Update generate color

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const cors = require("cors");
+const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
 
 app.use(bodyParser.json());
-// app.use(cors());
+app.use(cors());
 
+// Parse element code
+let elementCode = [
+  { name: "earth", code: 0 },
+  { name: "wood", code: 1 },
+  { name: "fire", code: 2 },
+  { name: "metal", code: 3 },
+  { name: "water", code: 4 },
+];
 
+let colorTable = {
+  0: [1, 4, 16, 12, 15, 13, 8, 7],
+  1: [3, 8, 16, 11, 2, 7],
+  2: [1, 6, 16, 15, 2, 8],
+  3: [1, 5, 11, 2, 9, 16, 7],
+  4: [1, 2, 11, 14, 8, 7, 13],
+};
 
 app.get("/bazi-god", (req, res, next) => {
   let date = req.query.date;
@@ -20,8 +35,8 @@ app.get("/bazi-god", (req, res, next) => {
   }
   axios
     .get(`http://35.187.245.238:5101/api/bazi/${date}/${time}`)
-    .then((respond) => {
-      let data = respond.data;
+    .then((response) => {
+      let data = response.data;
       res.send(getGodCount(data, haveHour));
     });
 });
@@ -39,9 +54,185 @@ app.get("/bazi-element", (req, res, next) => {
     .then((response) => {
       let data = response.data;
       res.send(getElementCount(data, haveHour));
-      resolve()
+      resolve();
     });
-})
+});
+
+app.get("/get-user-color", (req, response, next) => {
+  let userID = req.query.user_id;
+  axios
+    .get(`http://35.187.245.238:5102/api/admin/users/${userID}`)
+    .then(res => {
+      console.log("----------------")
+      console.log("user id  = " + userID)
+      let userData = res.data;
+      let dateOfBirth = userData.date_of_birth;
+      let timeOfBirth = userData.time_of_birth;
+      let haveHour = 1;
+      if (timeOfBirth === "") {
+        timeOfBirth = "null";
+        haveHour = 0
+      }
+      let promises = [];
+      let userElement
+      promises.push(
+        axios
+        .get(`http://35.187.245.238:5101/api/bazi/${dateOfBirth}/${timeOfBirth}`)
+        .then(response => {
+          let natal = response.data;
+          userElement = countElement(natal, haveHour);
+          console.log('user', userElement)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      );
+
+      let today = new Date();
+      today = today.toISOString().slice(0, 10);
+      let todayElement
+
+      promises.push(
+        axios.get(`http://35.187.245.238:5101/api/bazi/${today}/null`)
+        .then(response => {
+          let natal = response.data;
+          todayElement = countElement(natal, 0)
+          console.log('today', todayElement);
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      );
+
+    Promise.all(promises).then(() => {
+      let sum = userElement.map( (userElement, index) => {
+        return Number((userElement + todayElement[index]).toFixed(3));
+      });
+      console.log("user + today ", sum)
+      let colorArray = getColor(sum);
+      console.log("color output", colorArray)
+      response.send(colorArray)
+      console.log('finished')
+    })
+    .catch(err => {
+      console.log(error)
+    });
+    })
+    .catch(err => {
+      response.send("User Not Found")
+    });
+    
+});
+
+const countElement = (natal, haveHour) => {
+
+  let percentArray = [0, 0, 0, 0, 0]
+
+  if (haveHour === 1) {
+    percentArray[natal.hour.heaven_element.code % 5] += 10
+  }
+  percentArray[natal.day.heaven_element.code % 5] += 10
+
+  percentArray[natal.month.heaven_element.code % 5] += 10
+
+  percentArray[natal.year.heaven_element.code % 5] += 10
+
+  if (haveHour === 1) {
+  percentArray[natal.hour.earth_element.code % 5] += 10
+  }
+  percentArray[natal.day.earth_element.code % 5] += 10
+  percentArray[natal.month.earth_element.code % 5] += 10
+  percentArray[natal.year.earth_element.code % 5] += 10
+
+
+  if (haveHour === 1) {
+    let computedCap = getCap(natal.hour.hidden_stem)
+    natal.hour.hidden_stem.forEach((hour) => {
+      percentArray[hour.element.code % 5] += computedCap;
+    });
+  }
+
+  computedCap = getCap(natal.day.hidden_stem)
+  natal.day.hidden_stem.forEach((day) => {
+    percentArray[day.element.code % 5] += computedCap;
+  });
+  computedCap = getCap(natal.month.hidden_stem)
+  natal.month.hidden_stem.forEach((month) => {
+    percentArray[month.element.code % 5] += computedCap;
+  });
+  computedCap = getCap(natal.year.hidden_stem)
+  natal.year.hidden_stem.forEach((year) => {
+    percentArray[year.element.code % 5] += computedCap;
+  });
+
+  // percentArray = percentArray.map(element => {
+  //   return Number(element.toFixed(3));
+  // })
+
+  return percentArray
+  // let result = getColor(percentArray)
+};
+
+const getCap = (hiddenStemArray) => {
+  return Number((5 / hiddenStemArray.length).toFixed(3));
+};
+
+const getColor = (countedElement) => {
+  // หาอินเวอร์ส
+  let elementData = countedElement.map((element, index) => {
+    return {
+      code: index,
+      value: (element * 100 - 100) * -1,
+    };
+  });
+  // เรียงจากมากไปน้อย
+  elementData = elementData.sort((a, b) => {
+    return b.value - a.value;
+  });
+  // หาผลรวม
+  let sum = 0;
+  elementData.forEach((data) => {
+    sum = sum + data.value;
+  });
+  // หาอัตราส่วน
+  let proportion = sum / 3;
+  let result = [];
+  // หาธาตุที่ต้องเติม
+  for (let i = 0, count = 0; i < elementData.length && count < 3; i++) {
+    let n = Math.ceil(elementData[i].value / proportion);
+    for (let j = 0; j < n; j++) {
+      result.push(elementCode[elementData[i].code].code);
+      // เอาแค่ 3 ตัว ถ้ามี 4 ตัว
+      result.length === 4 ? result.pop() : null;
+      count++;
+    }
+  }
+
+  console.log("---------------")
+  console.log("element", result)
+
+  let range = [0,0,0,0,0];
+  let ans = []
+  let a = result.map(code => {
+    range[code] += Math.floor(Math.random() * colorTable[code].length);
+    // console.log(range)
+
+    if(ans.includes(colorTable[code][range[code] % colorTable[code].length])){
+      console.log("Found Duplicate Color!! Re-Randoming")
+      while(ans.includes(colorTable[code][range[code] % colorTable[code].length])){
+        range[code] += Math.floor(Math.random() * colorTable[code].length);
+      }
+    }
+
+    ans.push(colorTable[code][range[code] % colorTable[code].length])
+
+    return  colorTable[code][range[code] % colorTable[code].length] 
+    
+  })
+
+  return a
+
+};
 
 const getGodCount = (natal, haveHour) => {
   let godCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -79,46 +270,62 @@ const getGodCount = (natal, haveHour) => {
     parseFloat((element / Math.max(...godCount)).toFixed(3))
   );
   return godCount;
-}
+};
+
+
 
 const getElementCount = (natal, haveHour) => {
-  elementCount = [0, 0, 0, 0, 0];
-  //output
-  //0 = earth
-  //1 = wood
-  //2 = fire
-  //3 = metal
-  //4 = water
-  if (haveHour == 1) {
-    elementCount[natal.hour.heaven_element.code % 5]++;
+  let percentArray = [0, 0, 0, 0, 0];
+  // Check if Natal Chart have Hour (animal.code = 0 mean there is no data )
+
+  if (haveHour === 1) {
+    // this.elementCount[natal.hour.heaven_element.code % 5]++;
+    percentArray[natal.hour.heaven_element.code % 5] += 10;
   }
-  elementCount[natal.day.heaven_element.code % 5]++;
-  elementCount[natal.month.heaven_element.code % 5]++;
-  elementCount[natal.year.heaven_element.code % 5]++;
-  if (haveHour == 1) {
-    elementCount[natal.hour.earth_element.code % 5]++;
+  // this.elementCount[natal.day.heaven_element.code % 5]++;
+  percentArray[natal.day.heaven_element.code % 5] += 10;
+
+  // this.elementCount[natal.month.heaven_element.code % 5]++;
+  percentArray[natal.month.heaven_element.code % 5] += 10;
+
+  // this.elementCount[natal.year.heaven_element.code % 5]++;
+  percentArray[natal.year.heaven_element.code % 5] += 10;
+
+  if (haveHour === 1) {
+    // this.elementCount[natal.hour.earth_element.code % 5]++;
+    percentArray[natal.hour.earth_element.code % 5] += 10;
   }
-  elementCount[natal.day.earth_element.code % 5]++;
-  elementCount[natal.month.earth_element.code % 5]++;
-  elementCount[natal.year.earth_element.code % 5]++;
-  if (haveHour == 1) {
+  // this.elementCount[natal.day.earth_element.code % 5]++;
+  percentArray[natal.day.earth_element.code % 5] += 10;
+  // this.elementCount[natal.month.earth_element.code % 5]++;
+  percentArray[natal.month.earth_element.code % 5] += 10;
+  // this.elementCount[natal.year.earth_element.code % 5]++;
+  percentArray[natal.year.earth_element.code % 5] += 10;
+
+  let computedCap = getCap(natal.hour.hidden_stem);
+  if (haveHour === 1) {
     natal.hour.hidden_stem.forEach((hour) => {
-      elementCount[hour.element.code % 5]++;
+      // this.elementCount[hour.element.code % 5]++;
+      percentArray[hour.element.code % 5] += computedCap;
     });
   }
+  computedCap = getCap(natal.day.hidden_stem);
   natal.day.hidden_stem.forEach((day) => {
-    elementCount[day.element.code % 5]++;
+    // this.elementCount[day.element.code % 5]++;
+    percentArray[day.element.code % 5] += computedCap;
   });
+  computedCap = getCap(natal.month.hidden_stem);
   natal.month.hidden_stem.forEach((month) => {
-    elementCount[month.element.code % 5]++;
+    // this.elementCount[month.element.code % 5]++;
+    percentArray[month.element.code % 5] += computedCap;
   });
+  computedCap = getCap(natal.year.hidden_stem);
   natal.year.hidden_stem.forEach((year) => {
-    elementCount[year.element.code % 5]++;
+    // this.elementCount[year.element.code % 5]++;
+    percentArray[year.element.code % 5] += computedCap;
   });
-  elementCount = elementCount.map((element) =>
-    parseFloat((element / Math.max(...elementCount)).toFixed(3))
-  );
-  return elementCount;
+
+  return percentArray;
 };
 
 app.use("/", (req, res, next) => {
